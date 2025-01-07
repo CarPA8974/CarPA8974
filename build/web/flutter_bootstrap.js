9ff50f389b454e0c6fbb231c1f85eb6099644fed
CarPA8974 <cpalvin@outlook.com>
2025-01-07 14:26:28 -0300

    remove unused build artifacts and update service worker asset references

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "481296704"
+    serviceWorkerVersion: "2384099299"
   }
 });
