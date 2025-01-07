62edb673cdcd86151f7b3b9ac9f76ddfef52240f
CarPA8974 <cpalvin@outlook.com>
2025-01-07 14:35:30 -0300

    update service worker version and resource hashes

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2384099299"
+    serviceWorkerVersion: "2372095033"
   }
 });
